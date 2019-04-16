93e23e1354b279c04793a38519ae30380d1fefc0
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2019-04-16 16:39:50 +0300

    add axios, add users table from google sheet

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -7,12 +7,39 @@
             <page-title :icon="page.icon" :title="page.title"/>
             <v-card-text>
               <v-layout row wrap>
-                <v-flex xs12>
-                  <p v-if="userIsAdmin">You are an admin. You can edit content :)</p>
-                  <p v-else>You are not an admin. You cant edit content :(</p>
+                <v-flex xs12 v-if="userIsAdmin">
+                  <p>You are an admin. You can edit content :)</p>
                   <!-- <a v-if="userIsAdmin" href="https://app.netlify.com/start/deploy?repository=https://github.com/ihororlovskyi/yehorpopov-nuxt">
                     <img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify">
                   </a> -->
+                  <p>
+                    <ul>
+                      <li>
+                        гугл <a htef="https://docs.google.com/spreadsheets/d/1ppfxeSO4XCZMDZ6y0kk1-ZCaqyITUJCtgShj17I9isE/edit" target="_blank">табличка</a>
+                      </li>
+                      <li>
+                        сервис <a htef="https://sheetdb.io" target="_blank">sheetdb</a>
+                      </li>
+                    </ul>
+                  </p>
+                  <p>
+                    <table v-if="users">
+                      <caption>Users</caption>
+                      <tr>
+                        <th>id</th>
+                        <th>phone</th>
+                        <th>email</th>
+                      </tr>
+                      <tr v-for="i in users.values">
+                        <td>{{ i[0] }}</td>
+                        <td>{{ i[1] }}</td>
+                        <td>{{ i[2] }}</td>
+                      </tr>
+                    </table>
+                  </p>
+                </v-flex>
+                <v-flex xs12 v-else>
+                  <p>You are not an admin. You cant edit content :(</p>
                 </v-flex>
               </v-layout>
             </v-card-text>
@@ -24,14 +51,27 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     data () {
       return {
         page: {
           title: 'Admin',
           icon: 'mdi-security'
-        }
+        },
+        users: {},
+        id: '',
+        phone: '',
+        email: ''
       }
+    },
+    mounted () {
+      axios.get('https://sheets.googleapis.com/v4/spreadsheets/1ppfxeSO4XCZMDZ6y0kk1-ZCaqyITUJCtgShj17I9isE/values/A2:C100?key=AIzaSyBe9QRJoT1Cm90GZDf4HRNNfz0pvSRscoE')
+        .then((response) => {
+          this.users = response.data
+          this.id = response.data.values.length
+        })
     },
     computed: {
       userIsAdmin () {
